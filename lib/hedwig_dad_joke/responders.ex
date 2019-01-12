10724d18723094897d8af60b8d4adaa08093dabe
max beizer <max.beizer@gmail.com>
2019-01-12 17:39:10 -0600

    Fallback to real joke module

--- lib/hedwig_dad_joke/responders.ex
+++ lib/hedwig_dad_joke/responders.ex
@@ -3,7 +3,7 @@ defmodule HedwigDadJoke.Responders do
   Responders for the dad joke plugin.
   """
   use Hedwig.Responder
-  @joke_module Application.get_env(:hedwig_dad_joke, :joke_module)
+  @joke_module Application.get_env(:hedwig_dad_joke, :joke_module, HedwigDadJoke)
 
   respond ~r/(dadjoke|dad joke)(\s*me)?$/iu, msg do
     response = @joke_module.random()
