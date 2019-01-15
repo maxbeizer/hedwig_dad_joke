846757af95e954f3cd9ec242db40a1c4868a27e5
Max Beizer <mbeizer@heroku.com>
2019-01-14 20:33:24 -0600

    Add @usage and README info (#5)

--- lib/hedwig_dad_joke/responders.ex
+++ lib/hedwig_dad_joke/responders.ex
@@ -5,6 +5,9 @@ defmodule HedwigDadJoke.Responders do
   use Hedwig.Responder
   @joke_module Application.get_env(:hedwig_dad_joke, :joke_module, HedwigDadJoke)
 
+  @usage """
+  hedwig dad joke - Replies with a joke that will make your eyes roll
+  """
   respond ~r/(dadjoke|dad joke)(\s*me)?$/iu, msg do
     response = @joke_module.random()
     send(msg, response)
